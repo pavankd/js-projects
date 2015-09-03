8cb2c030a0ed6715d450d30226eb77f8b45bbfb3
pavankd <pavank.d@gmail.com>
2015-09-03 17:25:34 +0530

    todoexample added
    underscore is not working need to check the angular filters

---
+++ angular-practo/todoeg/todo.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp', []);
+app.controller('TodoCtrl', ['$scope', function ($scope) {
+	
+    $scope.todos = [{text:"Learn AngularJS",done:false},
+                    {text:'Build an app',done:false}];
+   $scope.getTotalTodos = function(){
+   	return $scope.todos.length;
+   };
+
+   $scope.trash = function(){
+   		$scope.todos = _.filter($scope.todos,function(todo){
+   			return !todo.done;
+   		});
+   };
+
+    $scope.addTodo = function(){
+    	
+    	$scope.todos.push({text:$scope.formTodoText,done:false});
+    	$scope.formTodoText='';
+    	
+    };
+}])
